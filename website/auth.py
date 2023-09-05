a5638dafc8e615329ec76fb0d2403ff803ad2434
Teeniest <120422510+Teeniest@users.noreply.github.com>
2023-09-04 19:40:41 -0600

    Creación de cuentas lista en python

--- website/auth.py
+++ website/auth.py
@@ -1,12 +1,11 @@
-from flask import Blueprint, render_template, request
+from flask import Blueprint, render_template, request, flash
 
 auth = Blueprint('auth',__name__)
 
 #Es necesario agregar el prefijo "/auth" para acceder a las sig. rutas: 
 @auth.route("/login",methods=['GET','POST'])
 def login():
-    data = request.form
-    print(data)
+    
     return render_template("login.html", cuenta="Hola", boolean=True)
 
 @auth.route("/log-out")
@@ -15,4 +14,21 @@ def logout():
 
 @auth.route("/sign-up", methods=['GET','POST'])
 def signup():
+    #Añadiendo usuario a base de datos
+    if request.method == 'POST':
+        email = request.form.get('email')
+        first_name = request.form.get('primer nombre')
+        password = request.form.get('password')
+        password2 = request.form.get('password2')
+
+        if len(email)<4:
+            flash('Correo es menor a 4 caracteres', category='error')
+        elif len(first_name)<2:
+            flash('Nombre debe ser mayor a 2 caracteres', category='error')
+        elif password != password2:
+            flash('Las contraseñas no coinciden', category='error')
+        elif len(password)<8:
+            flash('La contraseña es menor a 8 caracteres', category='error')
+        else:
+            flash('Cuenta creada exitosamente', category='succes')
     return render_template("sign_up.html")
