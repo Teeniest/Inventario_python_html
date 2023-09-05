ceb55bc2cb6e23e051637ed25df1ab8f879812df
Teeniest <120422510+Teeniest@users.noreply.github.com>
2023-09-04 19:30:32 -0600

    post get y guardado de datos

--- website/auth.py
+++ website/auth.py
@@ -1,16 +1,18 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request
 
 auth = Blueprint('auth',__name__)
 
 #Es necesario agregar el prefijo "/auth" para acceder a las sig. rutas: 
-@auth.route("/login")
+@auth.route("/login",methods=['GET','POST'])
 def login():
+    data = request.form
+    print(data)
     return render_template("login.html", cuenta="Hola", boolean=True)
 
 @auth.route("/log-out")
 def logout():
     return render_template("logout.html")
 
-@auth.route("/sign-up")
+@auth.route("/sign-up", methods=['GET','POST'])
 def signup():
     return render_template("sign_up.html")
