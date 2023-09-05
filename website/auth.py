af5ea26d4c33f30d58b4a5023a7e1ccc20e5e40e
Teeniest <120422510+Teeniest@users.noreply.github.com>
2023-09-04 19:16:22 -0600

    Formularios de html para login y signup

--- website/auth.py
+++ website/auth.py
@@ -1,16 +1,16 @@
-from flask import Blueprint
+from flask import Blueprint, render_template
 
 auth = Blueprint('auth',__name__)
 
 #Es necesario agregar el prefijo "/auth" para acceder a las sig. rutas: 
 @auth.route("/login")
 def login():
-    return "<p>Login</p>"
+    return render_template("login.html", cuenta="Hola", boolean=True)
 
 @auth.route("/log-out")
 def logout():
-    return "<p>Logout</p>"
+    return render_template("logout.html")
 
 @auth.route("/sign-up")
 def signup():
-    return "<p>Sign up</p>"
+    return render_template("sign_up.html")
