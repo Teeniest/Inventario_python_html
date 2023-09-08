eca726e69c66dd0965c0bbbae518822ac1d9b074
Teeniest <120422510+Teeniest@users.noreply.github.com>
2023-09-08 08:48:34 -0600

    incio y cierre de sesiones

--- website/auth.py
+++ website/auth.py
@@ -1,18 +1,32 @@
 from flask import Blueprint, render_template, request, flash, redirect, url_for
 from .models import User
 from werkzeug.security import generate_password_hash, check_password_hash
+from . import db
+from flask_login import login_user, login_required, logout_user, current_user
 
 auth = Blueprint('auth',__name__)
 
 #Es necesario agregar el prefijo "/auth" para acceder a las sig. rutas: 
 @auth.route("/login",methods=['GET','POST'])
 def login():
-    
-    return render_template("login.html", cuenta="Hola", boolean=True)
+    if request.method == 'POST':
+        email = request.form.get('email')
+        password = request.form.get('password')
+        user = User.query.filter_by(email=email).first()
+        if user:
+            if check_password_hash(user.password, password):
+                flash('Acceso a cuenta correcto', category='success')
+                login_user(user, remember=True)
+                return redirect(url_for('views.home'))
+            else:
+                flash('Contraseña incorrecta, vuelve a intentar.', category='error')
+        else:
+            flash('El usuario no existe',category='error')
+    return render_template("login.html", boolean=True)
 
 @auth.route("/log-out")
 def logout():
-    return render_template("logout.html")
+    return redirect(url_for('views.home'))
 
 @auth.route("/sign-up", methods=['GET','POST'])
 def signup():
@@ -23,7 +37,10 @@ def signup():
         password = request.form.get('password')
         password2 = request.form.get('password2')
 
-        if len(email)<4:
+        user = User.query.filter_by(email=email).first()
+        if user:
+            flash('El correo ya fue registrado','category='error')
+        elif len(email)<4:
             flash('Correo es menor a 4 caracteres', category='error')
         elif len(first_name)<2:
             flash('Nombre debe ser mayor a 2 caracteres', category='error')
@@ -35,6 +52,7 @@ def signup():
             new_user = User(email=email, firstname=first_name, password=generate_password_hash(password, method='shas56'))
             db.session_add(new_user)
             db.session.commit()
+            login_user(user, remember=True)
             flash('Cuenta creada exitosamente', category='succes')
             return redirect(url_for('views.home'))
 
